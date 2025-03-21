5332ac00111b3ed904ae504c7015f89420b388ff
yusufwahyupratama <yusufwahyu420@gmail.com>
2025-03-21 14:34:08 +0700

    first commit

---
+++ tes.py
@@ -0,0 +1,28 @@
+import qrcode
+from qrcode.image.styledpil import StyledPilImage
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=2,
+)
+
+url = input('insert URL or text: ')
+user_have_logo = input("want to add a logo to your QR code? (Y/N): ").strip().lower()
+qr.add_data(url)
+qr.make(fit=True)
+
+if user_have_logo == 'y':
+    logo = input("Insert your logo file name: ")
+    qr_img = qr.make_image(image_factory=StyledPilImage, embedded_image_path=logo)
+else:
+    custom_color = input("Want to use custom colors on your QR code? (Y/N): ").strip().lower()
+    if custom_color == 'y':
+        foreground = input("foreground color: ")
+        background = input("background color: ")
+        qr_img = qr.make_image(fill_color=foreground, back_color=background)
+    else:
+        qr_img = qr.make_image(fill_color="black", back_color="white")
+
+qr_img.save('qrcode.png')
